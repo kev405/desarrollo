e33583f8c2cbab4853fcf40615f85791fe16f7b5
Kev407 <karistizabal307@gmail.com>
2024-06-10 16:16:19 -0500

    one more time

--- vue/src/store/user.js
+++ vue/src/store/user.js
@@ -2,7 +2,7 @@ import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useUserStore = defineStore('user', () => {
-    const apiUrl = ref('http://127.0.0.1:8000/api/')
+    const apiUrl = ref('http://34.66.141.23:8000/api/')
 
     const isLoged = ref(false)
     const userInfo = ref({})
