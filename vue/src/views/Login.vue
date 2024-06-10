097b827415321db0ffac7b79a7f6c90dc38d6acf
Kev407 <karistizabal307@gmail.com>
2024-06-10 17:30:07 -0500

    hook prueba

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -94,7 +94,7 @@
           <Button
             @click="signIn()"
             class="w-full"
-            label="Iniciar sesión otra prueba hook"
+            label="Iniciar sesión"
             icon="pi pi-user"
             id="sign-in-button"
             :loading="signInLoading"
