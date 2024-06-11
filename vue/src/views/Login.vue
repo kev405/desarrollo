087e02a3fa8f19eeb6e35737f8d3c85002eff3be
Kev407 <karistizabal307@gmail.com>
2024-06-10 22:39:58 -0500

    here we go again

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -94,7 +94,7 @@
           <Button
             @click="signIn()"
             class="w-full"
-            label="Iniciar sesión otra vez"
+            label="Iniciar sesión otra vez marditasea"
             icon="pi pi-user"
             id="sign-in-button"
             :loading="signInLoading"
